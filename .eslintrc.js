17e83bb078e65b136261dcba75b592893a70259d
ejmudrak <ejmudrak@gmail.com>
2018-01-22 01:08:59 -0500

    Configured linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "comma-dangle": ["warn", "never"],
+        "max-len": ['warn', 500],
+        "semi": ["warn", "never"],
+        'react/prefer-stateless-function': ['warn', { ignorePureComponents: true }],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "import/prefer-default-export": 'off',
+        'import/no-named-as-default-member': 'off',
+        "no-case-declarations": 'off',
+    },
+    "extends": "airbnb"
+};
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import * as exampleActions from '../redux/actions/creators/exampleActions'
-import * as accountActions from '../redux/actions/creators/accountActions'
-import HomePage from '../presentation/home/homePage'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+// import { withRouter } from 'react-router-dom'
+// import * as exampleActions from '../redux/actions/creators/exampleActions'
+// import HomePage from '../presentation/home/homePage'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <HomePage {...this.props} />
-    )
-  }
-}
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <HomePage {...this.props} />
+//     )
+//   }
+// }
 
-Home.propTypes = {
-  signIn: PropTypes.func.isRequired
-}
+// Home.propTypes = {
+//   signIn: PropTypes.func.isRequired
+// }
 
-export default withRouter(connect(
-  state => ({ home: state.home, account: state.account }),
-  { ...exampleActions, ...accountActions },
-)(Home))
+// export default withRouter(connect(
+//   state => ({ home: state.home }),
+//   { ...exampleActions },
+// )(Home))
